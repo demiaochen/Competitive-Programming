66c24d843958d4264f982da6627c0277abe8f739
Mayo <56354990+memayo@users.noreply.github.com>
2021-10-02 14:53:39 +0800

    add my competitive programming template v1.0

---
+++ zcp_template.cpp
@@ -0,0 +1,31 @@
+//# include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <climits>  // INT_MAX  INT_MIN
+#include <utility>  // swap(), make_pair()
+#include <cmath>    // pow()
+#define vt vector
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+#define FOR(n) for (int i = 0; i < n; ++i)
+#define FORR(i, n) for (int i = 0; i < n; ++i)
+#define FORRR(i, m, n) for (int i = m; i < n; ++i)
+#define INF 1000000007  // 1e9+7
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define cout(a) std::cout << a << "\n"
+using ll = long long;        //typedef long long    ll;  
+using ld = long double;      //typedef long double  ld;  
+using namespace std;
+
+
+void solve() {
+}
+
+int main() {
+    fastIO();
+
+    
+}       
