0e7b00d7a4f442e2bc549f5b2b70cd884954720b
Les quatre cents coups <ssqr37@gmail.com>
2022-12-05 21:13:36 +1100

    template 1.1

--- zcp_template.cpp
+++ zcp_template.cpp
@@ -1,31 +1,54 @@
-//# include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 #include <iostream>
 #include <cstdio>
-#include <algorithm>
-#include <vector>
+#include <algorithm> // fill(first, last, value)
+#include <vector>    // vec.assign(n, -1);
 #include <string>
+#include <cstring>  // memset()
 #include <climits>  // INT_MAX  INT_MIN
 #include <utility>  // swap(), make_pair()
-#include <cmath>    // pow()
+#include <cmath>    // pow()   2^n == 1<<n  x<<k x*2^k  1LL << k  1ULL << k
+#include <set>
+#include <map>
+#include <queue>
+
+#ifdef DEBUG
+    #define D(x...) printf(x)
+#else  
+    #define D(x...)
+#endif
+
+// #define int int64_t
+using namespace std;
+
+using ll = long long;        //typedef long long    ll;  
+using ld = long double;      //typedef long double  ld;  
+using ull = unsigned long long;
+typedef pair<ll, ll> pii;
 #define vt vector
 #define ALL(v) v.begin(), v.end()
 #define SORT(v) sort(ALL(v))
-#define FOR(n) for (int i = 0; i < n; ++i)
-#define FORR(i, n) for (int i = 0; i < n; ++i)
-#define FORRR(i, m, n) for (int i = m; i < n; ++i)
-#define INF 1000000007  // 1e9+7
-#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define cout(a) std::cout << a << "\n"
-using ll = long long;        //typedef long long    ll;  
-using ld = long double;      //typedef long double  ld;  
-using namespace std;
+#define MEM(a,b) memset(a,b,sizeof a)
+
+#define REP(n) for (int i = 0; i < n; ++i)          // repeat n times, i as index
+#define REPP(i, n) for (int i = 0; i < n; ++i)      // repeat n times, arg0 as index
+#define FO(m, n) for (int i = m; i <= n; ++i)       // from m to n, ++i
+#define FOV(m, n) for (int i = m; i >= n; --i)      // from m to n, --i
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)   
+#define FORV(i, a, b) for (int i = a; i >= b; --i)
+
+#define INF 0x3f3f3f3f  // 1e9
+#define INFF 0x3f3f3f3f3f3f3f3f  // 1e18
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(nullptr); 
+#define cout(a) std::cout << a << "\n";
+#define coutt(a) std::cout << a << " ";
+
+#define MAXN 1000005 // 1e6 4MB
+const int N = 1e5 + 10;
+const int mod = 1e9 + 7;
 
 
-void solve() {
-}
-
-int main() {
+signed main() {
     fastIO();
 
-    
 }       
